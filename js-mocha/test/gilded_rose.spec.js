775386a45f43a89747a49116bb397565fe8a6ca4
Craig Rosen <Craig_Rosen@mednax.com>
2019-06-17 08:46:10 -0400

    Initial fix to package.json and to test file

--- js-mocha/test/gilded_rose.spec.js
+++ js-mocha/test/gilded_rose.spec.js
@@ -1,11 +1,15 @@
-var {expect} = require('chai');
+var {expect, should} = require('chai');
 var {Shop, Item} = require('../src/gilded_rose.js');
+
+should();
+
 describe("Gilded Rose", function() {
 
   it("should foo", function() {
     const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).to.equal("fixme");
+    items[0].name.should.equal('foo');
+    //expect(items[0].name).to.equal("fixme");
   });
 
 });
